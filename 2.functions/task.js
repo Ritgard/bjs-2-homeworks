7c193f6b7991da7d09b9638ac240caacfffc9b1f
Ritgard <m0untlab@yandex.ru>
2022-06-25 12:57:57 +0300

    Решена первая задача

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,48 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+    let min, max, sum, avg;
+    min = Infinity;
+    max = -Infinity;
+    sum = 0;
 
-  // Ваш код
+    for (let i = 0; i < arr.length; i++) {
+        if (min > arr[i]) {
+            // если минимум меньше элемента массива, то минимум становится равен элементу
+            min = arr[i];
+        }
+        if (max < arr[i]) {
+            // если максимум больше элемента массива, то максимум становится равен элементу
+            max = arr[i];
+        }
+        sum = sum + arr[i];
+    }
+    avg = sum / arr.length; // среднее значение
+    avg = parseFloat(avg.toFixed(2));
+    console.log(typeof avg);
 
-  return { min: min, max: max, avg: avg };
+    return { min: min, max: max, avg: avg };
 }
+console.log(getArrayParams([-99, 99, 10]));
 
 // Задание 2
 function worker(arr) {
-  let sum;
+    let sum;
 
-  // Ваш код
+    // Ваш код
 
-  return sum;
+    return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+    let max;
 
-  // Ваш кода
-  // for ...
-  
-  return max;
+    // Ваш кода
+    // for ...
+
+    return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    // Ваш код
 }
