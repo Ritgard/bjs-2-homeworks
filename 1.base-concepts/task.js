f2ea7b745e9d4c67c19c7a1bb9c9264d93c5085b
Ritgard <m0untlab@yandex.ru>
2022-06-25 11:10:52 +0300

    Решена первая задача

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,24 @@
+'use strict';
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let arr = [];
+    let discriminant = Math.pow(b, 2) - 4 * a * c; // переменная для вычисления дискриминанта
+    if (discriminant === 0) {
+        // если дискриминант равен нулю, то возвращаем один корень
+        let square = -b / (2 * a);
+        arr.push(square);
+    } else if (discriminant > 0) {
+        // если дискриминант больше нуля
+        let square = (-b + Math.sqrt(discriminant)) / (2 * a); // первый корень
+        let squareTwo = (-b - Math.sqrt(discriminant)) / (2 * a); // второй корень
+        arr.push(square, squareTwo);
+    }
+    return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    let totalAmount;
 
-  // код для задачи №2 писать здесь
+    // код для задачи №2 писать здесь
 
-  return totalAmount;
+    return totalAmount;
 }
