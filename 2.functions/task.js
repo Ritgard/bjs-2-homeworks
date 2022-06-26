885d6b43b252b9e9d34828774130027d71ff58fb
Ritgard <m0untlab@yandex.ru>
2022-06-26 15:58:43 +0300

    Поменял циклы for, на цикл for in, отредактировал код

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -5,16 +5,12 @@ function getArrayParams(arr) {
     max = -Infinity;
     sum = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        if (min > arr[i]) {
-            // если минимум меньше элемента массива, то минимум становится равен элементу
-            min = arr[i];
-        }
-        if (max < arr[i]) {
-            // если максимум больше элемента массива, то максимум становится равен элементу
-            max = arr[i];
-        }
-        sum = sum + arr[i];
+    for (item of arr) {
+        // если минимум меньше элемента массива, то минимум становится равен элементу
+        if (min > item) min = item;
+        if (max < item) max = item;
+        // если максимум больше элемента массива, то максимум становится равен элементу
+        sum = sum + item;
     }
     avg = sum / arr.length; // среднее значение
     avg = parseFloat(avg.toFixed(2));
@@ -25,9 +21,9 @@ function getArrayParams(arr) {
 // Задание 2
 function worker(arr) {
     let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
+    for (item of arr) {
         // в теле цикла складываются все числа массива и записываются в переменную sum
-        sum = sum + arr[i];
+        sum += item;
     }
     return sum;
 }
@@ -37,11 +33,8 @@ function makeWork(arrOfArr, func) {
 
     for (let i = 0; i < arrOfArr.length; i++) {
         // цикл проходится по всем массивам вычисляя с помощью функции worker сумму массива, если она больше предыдущей, то это число записывается в переменную max
-        if (func(arrOfArr[i]) > max) {
-            max = func(arrOfArr[i]);
-        }
+        if (func(arrOfArr[i]) > max) max = func(arrOfArr[i]);
     }
-
     return max;
 }
 
@@ -51,15 +44,11 @@ function worker2(arr) {
     let max = -Infinity;
     let difference = 0; // разность чисел
 
-    for (let i = 0; i < arr.length; i++) {
-        if (min > arr[i]) {
-            // если минимум меньше элемента массива, то минимум становится равен элементу
-            min = arr[i];
-        }
-        if (max < arr[i]) {
-            // если максимум больше элемента массива, то максимум становится равен элементу
-            max = arr[i];
-        }
+    for (item of arr) {
+        if (min > item) min = item;
+        // если минимум меньше элемента массива, то минимум становится равен элементу
+        if (max < item) max = item;
+        // если максимум больше элемента массива, то максимум становится равен элементу
     }
     difference = max - min;
     difference = Math.abs(difference);
